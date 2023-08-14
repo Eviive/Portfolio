4b2d08beb862df6149cafe3ba0b500d847567b82
Eviive <albert.vaillon21@gmail.com>
2023-08-14 19:26:34 +0200

    feat: changed the way calling a context without a provider works

--- src/contexts/I18nContext.tsx
+++ src/contexts/I18nContext.tsx
@@ -1,9 +1,14 @@
 import type { Locale } from "@/lib/i18n";
-import { defaultLocale } from "@/lib/i18n";
 import { createServerContext, useContext } from "react";
 
-const I18nContext = createServerContext<Locale>("I18nContext", defaultLocale);
+const I18nContext = createServerContext<Locale | null>("I18nContext", null);
 
 export const I18nContextProvider = I18nContext.Provider;
 
-export const useI18nContext = () => useContext(I18nContext);
+export const useI18nContext = () => {
+    const i18nContext = useContext(I18nContext);
+    if (!i18nContext) {
+        throw new Error("useI18nContext called without I18nContextProvider");
+    }
+    return i18nContext;
+};
