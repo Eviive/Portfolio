73a1608ccf63a9c9222752ef17f85f9bb2d48950
Eviive <albert.vaillon21@gmail.com>
2024-03-27 11:06:50 +0100

    fix: scroll-reveal with anchors

--- src/components/common/client/ScrollReveal/ScrollReveal.tsx
+++ src/components/common/client/ScrollReveal/ScrollReveal.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { isNotNullOrUndefined, isNullOrUndefined } from "@/libs/utils/assertion";
-import type { FC, ReactElement } from "react";
+import type { FC, ReactNode } from "react";
 import { useEffect, useRef } from "react";
 
 type SingleRevealProps = {
     multiple?: false;
-    content: ReactElement;
+    content: ReactNode;
 };
 type MultipleRevealProps = {
     multiple: true;
-    content: ReactElement[];
+    content: ReactNode[];
 };
 type Props = {
     options?: scrollReveal.ScrollRevealObjectOptions & {
@@ -25,53 +25,51 @@ export const ScrollReveal: FC<Props> = props => {
     useEffect(() => {
         let scrollRevealObject: scrollReveal.ScrollRevealObject;
 
+        const setUpScrollReveal = (el: HTMLElement, i: number) => {
+            const {
+                intervalDelay,
+                ...options
+            } = props?.options ?? {};
+
+            let delay = props.options?.delay ?? 100;
+
+            if (props.multiple) {
+                delay += i * (intervalDelay ?? 100);
+            }
+
+            scrollRevealObject.reveal(el, {
+                origin: "bottom",
+                duration: 500,
+                easing: "ease",
+                distance: "25%",
+                opacity: 0,
+                scale: 0.9,
+                viewFactor: 0.75,
+                cleanup: true,
+                ...options,
+                afterReveal: (el: HTMLElement) => {
+                    options?.afterReveal?.(el);
+                    scrollRevealObject.clean(el);
+                    el.removeAttribute("style");
+                    el.classList.remove("reveal-hidden");
+                },
+                delay: delay
+            });
+
+            el.dataset.revealed = "";
+        };
+
         (async () => {
             scrollRevealObject = (await import("scrollreveal")).default();
 
             refs.current
                 .filter(isNotNullOrUndefined)
                 .filter(el => isNullOrUndefined(el.dataset.revealed))
-                .forEach((el, i) => {
-                    const {
-                        intervalDelay,
-                        ...options
-                    } = props?.options ?? {};
-
-                    let delay = props.options?.delay ?? 100;
-
-                    if (props.multiple) {
-                        delay += i * (intervalDelay ?? 100);
-                    }
-
-                    scrollRevealObject.reveal(el, {
-                        origin: "bottom",
-                        duration: 500,
-                        easing: "ease",
-                        distance: "25%",
-                        opacity: 0,
-                        scale: 0.9,
-                        viewFactor: 0.75,
-                        cleanup: true,
-                        ...options,
-                        afterReveal: (el: HTMLElement) => {
-                            options?.afterReveal?.(el);
-                            scrollRevealObject.clean(el);
-                            el.removeAttribute("style");
-                            el.classList.remove("reveal-hidden");
-                        },
-                        delay: delay
-                    });
-
-                    el.dataset.revealed = "";
-                });
+                .forEach(setUpScrollReveal);
         })();
-
-        return () => {
-            scrollRevealObject?.destroy();
-        };
     }, [ props.content, props.multiple, props.options ]);
 
-    const wrapComponent = (component: ReactElement, i: number = 0) => (
+    const wrapComponent = (component: ReactNode, i: number = 0) => (
         <li
             key={i}
             ref={el => refs.current[i] = el}
