2074aacaa0b7e4ec9b2bac897b6aae684b6f7ec3
Eviive <albert.vaillon21@gmail.com>
2024-03-30 17:35:38 +0100

    feat: sourcemaps

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
         contentDispositionType: "attachment",
         contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
     },
+    productionBrowserSourceMaps: true,
     output: "standalone"
 };
 
