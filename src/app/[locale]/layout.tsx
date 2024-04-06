e4dc767038c1dda9e65f832624047a4a32db1f54
Eviive <albert.vaillon21@gmail.com>
2024-04-07 00:57:59 +0200

    fix: canonical URL

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -91,7 +91,6 @@ export const generateMetadata = ({
             }
         },
         alternates: {
-            canonical: baseUrl?.toString(),
             languages: localesUrl
         },
         manifest: "/manifest.json",
