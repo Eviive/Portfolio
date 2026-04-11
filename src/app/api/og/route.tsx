d429e0f19a4c4fae8941675249ddbe4c0786ef17
Albert Vaillon <albert.vaillon21@gmail.com>
2026-04-12 01:00:13 +0200

    fix: prettier

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -8,27 +8,25 @@ export const GET = async (): Promise<ImageResponse> => {
     const logoSrc = "data:image/svg+xml," + encodeURIComponent(logoData);
 
     return new ImageResponse(
-        (
-            <div
-                style={{
-                    width: "100%",
-                    height: "100%",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    gap: 50,
-                    background: "#1a1a1a",
-                    color: "#f2f2f2",
-                    fontSize: 40,
-                    fontFamily: "sans-serif"
-                }}
-            >
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src={logoSrc} alt="Albert Vaillon's logo" height={300} />
-                Albert Vaillon&apos;s Portfolio
-            </div>
-        ),
+        <div
+            style={{
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 50,
+                background: "#1a1a1a",
+                color: "#f2f2f2",
+                fontSize: 40,
+                fontFamily: "sans-serif"
+            }}
+        >
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src={logoSrc} alt="Albert Vaillon's logo" height={300} />
+            Albert Vaillon&apos;s Portfolio
+        </div>,
         {
             width: 1200,
             height: 630
